b88cfe5f40ba147a6d83c072599b22d44b3bcd76
Sebastián Galván <sebastiangalvan.ar@gmail.com>
2021-11-18 17:23:21 -0300

    [ADD] Router Definition > Post > Category

---
+++ app/routes/CategoryRoutes.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+
+const CategoryController =  require('../controllers/CategoryController')
+//Category
+
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Category:
+ *       type: object
+ *       required:
+ *         - description
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the category
+ *         description:
+ *           type: string
+ *           description: The category description
+ *       example:
+ *         id: 2
+ *         description : Diseño
+ */
+
+
+ /**
+  * @swagger
+  * tags:
+  *   name: Category
+  *   description: The categories managing API
+  */
+
+/**
+ * @swagger
+ * /category:
+ *   get:
+ *     summary: Returns the list of categories.
+ *     tags: [Category]
+ *     responses:
+ *       200:
+ *         description: The list of the categories
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Category'
+ */
+
+router.get('/', CategoryController.getCategories);
+
+module.exports = router;
---
+++ app/routes/PostRoutes.js
@@ -0,0 +1,171 @@
+const express = require('express');
+const router = express.Router();
+
+const PostsController =  require('../controllers/PostController')
+const validations =  require('../validations/PostValidation');
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Post:
+ *       type: object
+ *       required:
+ *         - title
+ *         - content
+ *         - image
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the post
+ *         title:
+ *           type: string
+ *           description: The post title
+ *         content:
+ *           type: string
+ *           description: The content of the post 
+ *         image:
+ *           type: string
+ *           description: The image of the post 
+ *       example:
+ *         id: 7
+ *         title: ¿Como comer una naranja?
+ *         content: Coloca la naranja sobre una tabla de cortar con la parte del tallo apuntando hacia arriba.
+ *         image: https://pbs.twimg.com/media/Dg6TRdsWkAA5NZl.jpg
+ */
+
+ /**
+  * @swagger
+  * tags:
+  *   name: Posts
+  *   description: The posts managing API
+  */
+
+
+/**
+ * @swagger
+ * /posts:
+ *   get:
+ *     summary: Returns the list of posts.
+ *     tags: [Posts]
+ *     responses:
+ *       200:
+ *         description: The list of the posts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Post'
+ */
+router.get('/', PostsController.getPosts);
+
+/**
+ * @swagger
+ * /posts/{id}:
+ *   get:
+ *     summary: Get the post by id
+ *     tags: [Posts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The post id
+ *     responses:
+ *       200:
+ *         description: The book description by id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ *       404:
+ *         description: The post was not found
+ */
+ router.get('/:id', PostsController.getPost);
+
+
+
+/**
+ * @swagger
+ * /posts:
+ *   post:
+ *     summary: Create a new post
+ *     tags: [Posts]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Post'
+ *     responses:
+ *       200:
+ *         description: The post was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ *       500:
+ *         description: Some server error
+ */
+router.post('/', validations.image ,PostsController.setPost);
+
+/**
+ * @swagger
+ * /posts/{id}:
+ *  patch:
+ *    summary: Update the post by the id
+ *    tags: [Posts]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The post id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Post'
+ *    responses:
+ *      200:
+ *        description: The post was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Post'
+ *      404:
+ *        description: The post was not found
+ *      500:
+ *        description: Some error happened
+ */
+router.patch('/:id', PostsController.updatePost);
+/**
+ * @swagger
+ * /posts/{id}:
+ *   delete:
+ *     summary: Remove the post by id
+ *     tags: [Posts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The post id
+ * 
+ *     responses:
+ *       200:
+ *         description: The post was deleted
+ *       404:
+ *         description: The post was not found
+ */
+router.delete('/:id', PostsController.deletePost);
+
+
+
+
+module.exports = router;
