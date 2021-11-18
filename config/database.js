0ac374d520da38bda26c6e957b2d5a69c17fb207
Sebastián Galván <sebastiangalvan.ar@gmail.com>
2021-11-18 18:48:46 -0300

    [ADD] - Database configuration

--- config/database.js
+++ config/database.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 module.exports = {
 
-    username : process.env.DB_USERNAME || "root",
-    password : process.env.DB_PASSWORD || "root",
+    username : process.env.DB_USERNAME,
+    password : process.env.DB_PASSWORD,
     database : process.env.DB_DATABASE || "alkemy-blog",
     host : process.env.DB_HOST || "127.0.0.1",
     dialect : process.env.DB_DIALECT || "mysql",
